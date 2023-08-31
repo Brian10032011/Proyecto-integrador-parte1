0b644049ca2b606bdfaa8ddb3154f477b39e11b1
Brian Vargas <brian_vargas_7@hotmail.com>
2023-08-30 21:38:25 -0500

    Realizando_cambios1

--- videojuego.py
+++ videojuego.py
@@ -1,4 +1,4 @@
 #pedir nombre del jugador por teclado
 nombre = input ("Por favor, ingresa tu nombre")
 #Mensaje de bienvenida con el nombre
-print ("Bienvenido Brian Vargas")
+print ("Bienvenido "+nombre)
