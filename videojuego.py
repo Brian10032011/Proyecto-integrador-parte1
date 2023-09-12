bcb7d474d8bf3c6130c1bebbafb64aa3c0086bf0
Brian Vargas <brian_vargas_7@hotmail.com>
2023-09-11 20:56:01 -0500

    Commit_parte_2

--- videojuego.py
+++ videojuego.py
@@ -1,4 +1,14 @@
 #pedir nombre del jugador por teclado
 nombre = input ("Por favor, ingresa tu nombre")
 #Mensaje de bienvenida con el nombre
-print ("Bienvenido "+nombre)
+print("Bienvenido "+nombre)
+
+
+from readchar import readkey, key
+
+while True:
+  k = readkey()
+  if k != key.UP:
+    print(k)
+  else:
+    break
